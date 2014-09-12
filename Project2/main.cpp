f230f65d72d3269c40f23ddd470fc7db484920ab
Natalia Eiré <natalia.eire.sommer@hotmail.co.uk>
2014-09-12 23:55:41 +0200

    Variables n and rho_max are now read from terminal.
    Some minor changes are made (mostly just making the code more
    consistent).
    The matrix R is now printed out to a file. HOWEVER, it does not give
    correct results o.O Something's wrong. Terribly wrong. Might be the
    rotation function, but I don't have the brains to look at it atm.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -11,31 +11,38 @@ using namespace arma;
 mat     set_matrix(int n, double h, vec &potential);
 void    create_rho_and_potential(int n, double h, double rho_min, vec &rho, vec &potential);
 void    jacobis_method(int n, mat &A, mat &R);
-double  maxoffdiagonal(int n, int &k, int &l, mat &A);
+double  max_offdiagonal(int n, int &k, int &l, mat &A);
 void    rotate(int n, int &k, int &l, mat &A, mat &R, double &max_off_diag);
 
 int main()
 {
     double h, rho_min, rho_max;
+    int n;
 
-    int n_step = 100;
-    int n = n_step;
-    mat A;
-    vec rho = zeros(n+2);                       // Vector saving the dimentionless radial positions
-    vec potential = zeros(n+2);                 // Vector saving the potential
-    mat R = eye(n,n);                           // Creating matrix R, which stores the eigenvectors of A
+    // Deciding on matrix and vector sizes and end points
+    rho_min = 0.;
+    cout << "Read in maximum value of dimensionless radius: ";
+    cin >> rho_max;
+    cout << "Read in matrix dimension: ";
+    cin >> n;
+    h = (rho_max-rho_min)/(n+1);            // Step size for radius vector
 
+    // Defining vectors and matrices
+    mat A;                                  // The Matrix with a large M which we are solving
+    vec rho = zeros(n+2);                   // Vector saving the dimentionless radial positions
+    vec potential = zeros(n+2);             // Vector saving the potential
+    mat R = eye(n,n);                       // Creating matrix R, which stores the eigenvectors of A
 
     // Calling functions to create matrix
-    rho_min = 0.;
-    rho_max = 10;
-    h = (rho_max-rho_min)/(n_step+1);
-    create_rho_and_potential(n, h, rho_min, rho, potential);          // Finding the potential at each point
+    create_rho_and_potential(n, h, rho_min, rho, potential);    // Finding the potential at each point
+    cout << rho << endl << endl;
+    cout << potential << endl;
     A = set_matrix(n, h, potential);
-    A.save("A.txt", raw_ascii);                 // Saving matrix to be able to have a look at it
+    A.save("A.txt", raw_ascii);             // Saving matrix to be able to have a look at it
 
     // Using Jacobi's method to solve the eigenvalue problem
     jacobis_method(n, A, R);
+    R.save("R.txt", raw_ascii);
 
     return 0;
 
@@ -49,14 +56,14 @@ void create_rho_and_potential(int n, double h, double rho_min,  vec &rho, vec &p
 }   // End create_potential-function
 
 mat set_matrix(int n, double h, vec &potential){
-    mat A(n,n);
-    A.zeros();
-    double offDiagonalValue = -1.0/h/h;
-    A.diag(-1).fill(offDiagonalValue);
-    A.diag(1).fill(offDiagonalValue);
+    mat A(n,n);                             // Creating matrix of correct size
+    A.zeros();                              // Initializing matrix, setting all elements to zero
+    double off_diagonal_value = -1.0/h/h;
+    A.diag(-1).fill(off_diagonal_value);    // Filling the elements below and above the diagonal with the
+    A.diag(1).fill(off_diagonal_value);     // same value, as given by the derivative
 
     for(int i = 0; i<n; i++){
-        A(i,i)   = 2/h/h + potential[i+1];
+        A(i,i) = 2/h/h + potential[i+1];    // Giving the diagonal elements values given by the derivative and potential
     }   // End for-loop
 
     return A;
@@ -66,23 +73,24 @@ mat set_matrix(int n, double h, vec &potential){
 void jacobis_method(int n, mat &A, mat &R){
     int k, l;
     int iteration_number = 0;
-    double max_iteration_number = n*n;    // We stop diagonalising the matrix after this to not kill something
-    double max_off_diag;
+    double max_iteration_number = n*n;      // We stop diagonalising the matrix after this to not kill something
+    double max_off_diag;                    // The maximum value of all off-diagonal terms
     double epsilon = pow(10,-8);            // If the maximum off-diagonal value is smaller than this,
                                             // we consider the matrix diagonalised
 
-    max_off_diag = maxoffdiagonal(n, k, l, A);
-
+    // Performing Jacobi's method
+    max_off_diag = max_offdiagonal(n, k, l, A);
     while (fabs(max_off_diag) > epsilon && iteration_number <= max_iteration_number){
-        max_off_diag = maxoffdiagonal(n, k, l, A);
+        max_off_diag = max_offdiagonal(n, k, l, A);
         rotate(n, k, l, A, R, max_off_diag);
         iteration_number++;
     }   // End while-loop
 
-    // Print out error in case matrix doesn't get diagonalised, and the solution went to hell
+    // Print out error in case matrix doesn't get diagonalised and solution will not be correct
     if(fabs(max_off_diag) > epsilon && iteration_number == max_iteration_number){
         cout << "Matrix was not succesfully diagonalised." << endl;
     }   // End if-statement
+
 }   // End jacobis_method-function
 
 void rotate(int n, int &k, int &l, mat &A, mat &R, double &max_off_diag){
@@ -136,18 +144,20 @@ void rotate(int n, int &k, int &l, mat &A, mat &R, double &max_off_diag){
 
 }   // Ending rotate-function
 
-double maxoffdiagonal(int n, int &k, int &l, mat &A){
-    double maxval = 0.0;
-    for(int i=0; i<n; i++){
+double max_offdiagonal(int n, int &k, int &l, mat &A){
+    double max_value = 0.0;
+    for(int i=1; i<n; i++){
         for(int j=i+1; j<n; j++){
-            if(fabs(A(i,j)) > maxval){
-                maxval = fabs(A(i,j));
+            if(fabs(A(i,j)) > max_value){
+                max_value = fabs(A(i,j));
                 l = i;
                 k = l;
             } // End if-test
         } // End j-loop
     } // End i-loop
-    return maxval;
-} // End maxoffdiagonal-function
+
+    return max_value;
+
+} // End max_offdiagonal-function
 
 
